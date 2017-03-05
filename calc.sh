6946acd9c40da2df1db5d5c057ad93fc42dae92a
Christer Blomqvist <christer.blomqvist@gmail.com>
2017-03-05 15:38:45 +0100

    Adding testing adds complexity...

--- calc.sh
+++ calc.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -e
-mkdir -p output
-INPUT=upstream/output
+WORKDIR=$1
+if [ A == A${WORKDIR} ]
+then
+    WORKDIR=.
+fi
+mkdir -p ${WORKDIR}/output
+INPUT=${WORKDIR}/upstream/output
 inkomst=$(cat ${INPUT}/socarb.txt)
 res=$(echo "scale=0; (${inkomst} * 0.1021) / 1" | bc -l)
-echo ${res} > output/alderspensionsavgift.txt
+echo ${res} > ${WORKDIR}/output/alderspensionsavgift.txt
